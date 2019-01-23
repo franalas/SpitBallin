45a45d19ef1ed1cd5bcb5bc05bfc88f53c7611dd
Liam Stevenson <liam923@verizon.net>
2019-01-23 15:56:42 -0500

    Creates controlling interface for GameScene

--- Watch Extension/GameScene.swift
+++ Watch Extension/GameScene.swift
@@ -10,4 +10,44 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    /**
+    Moves the player left or right. Will stop at a wall.
+    - Parameters:
+      - value: how far to move the player
+    */
+    func movePlayer(_ value: Double) {
+        
+        movePlayerSprite(distance: CGFloat(value))
+        
+    }
+    
+    /**
+    Shoots shot.
+    */
+    func shoot() {
+        
+        shootFromPlayer()
+        
+    }
+    
+    /**
+    Moves the player the given distance. Stops at a wall.
+    - Parameters:
+      - distance: how far to move the player
+    */
+    private func movePlayerSprite(distance: CGFloat) {
+        
+        
+        
+    }
+    
+    /**
+    Shoots a shot upwards out of the player object.
+    */
+    private func shootFromPlayer() {
+        
+        
+        
+    }
+    
 }
