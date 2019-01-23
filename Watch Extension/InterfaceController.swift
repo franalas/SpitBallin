a276f399781a629249c965853309c9569eba22f8
Liam Stevenson <liam923@verizon.net>
2019-01-23 01:56:52 -0500

    Remove the GameScene.sks file

--- Watch Extension/InterfaceController.swift
+++ Watch Extension/InterfaceController.swift
@@ -21,18 +21,18 @@ class InterfaceController: WKInterfaceController {
         
         // Configure interface objects here.
         
-        // Load the SKScene from 'GameScene.sks'
-        if let scene = GameScene(fileNamed: "GameScene") {
-            
-            // Set the scale mode to scale to fit the window
-            scene.scaleMode = .aspectFill
-            
-            // Present the scene
-            self.skInterface.presentScene(scene)
-            
-            // Use a value that will maintain a consistent frame rate
-            self.skInterface.preferredFramesPerSecond = 30
-        }
+        // Create the SKScene
+        let scene = GameScene()
+        
+        // Set the scale mode to scale to fit the window
+        scene.scaleMode = .aspectFill
+        
+        // Present the scene
+        self.skInterface.presentScene(scene)
+        
+        // Use a value that will maintain a consistent frame rate
+        self.skInterface.preferredFramesPerSecond = 30
+        
     }
     
     override func willActivate() {
