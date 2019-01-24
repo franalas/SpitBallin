8f6bdc382127d61dfaded15d2d4204783ad26b22
Liam Stevenson <liam923@verizon.net>
2019-01-23 22:57:54 -0500

    Create BorderType enum

---
+++ Watch Extension/BorderType.swift
@@ -0,0 +1,20 @@
+//
+//  BorderType.swift
+//  Watch Extension
+//
+//  Created by Liam Stevenson on 1/23/19.
+//  Copyright © 2019 William Stevenson. All rights reserved.
+//
+
+import Foundation
+
+/// Represents a kind of screen border
+enum BorderType: String {
+    
+    /// A wall on the side of the screen
+    case wall = "wall"
+    
+    /// A floor on the bottom of the screen
+    case floor = "floor"
+    
+}
