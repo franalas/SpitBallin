cef506e47f8e82f164555afa70a5b3992acbda6a
Francisco Turdera <franciscoturdera@hotmail.com>
2019-01-24 16:11:08 -0500

    Finalized back-end of Bullet, added spit image

--- Watch Extension/Bullet.swift
+++ Watch Extension/Bullet.swift
@@ -11,8 +11,11 @@ import SpriteKit
 
 class Bullet: SKSpriteNode {
     
-    /// Represents the Center Point of the Bullet
-    private let CENTER: CGPoint = CGPoint(x: 0.5, y: 0.5)
+//    /// Represents the Center Point of the Bullet
+//    private let CENTER: CGPoint = CGPoint(x: 0.5, y: 0.5)
+    
+    /// Represents the image of the Bullet
+    private var standardTexture: SKTexture?
     
     /**
      Initializes a Bullet, used to interact solely with Ball
@@ -23,15 +26,18 @@ class Bullet: SKSpriteNode {
      */
     convenience init(size: CGSize, position: CGPoint, velocity: CGVector) {
         
-        self.init()
+        let standardTexture = SKTexture(imageNamed: "spit")
+        
+        self.init(texture: standardTexture)
+        self.standardTexture = standardTexture
         self.size = size
-//        self.physicsBody = SKPhysicsBody(bodies: <#T##[SKPhysicsBody]#>) //Figure out the kind of input this requires
+        self.physicsBody = SKPhysicsBody(texture: self.standardTexture!, size: self.size)
         self.position = position
         self.physicsBody!.velocity = velocity
         
     }
     
-    /// Removes bullet by not returning nil when called
+    /// Removes bullet by returning nil when called
     func remove() -> Bullet? {
         
         return nil
