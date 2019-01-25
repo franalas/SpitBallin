23a0b7b4c64e74cab767bbe5a24e09e696e84e30
Francisco Turdera <franciscoturdera@hotmail.com>
2019-01-24 21:33:32 -0500

    Necessary additions to Bullet

--- Watch Extension/Game/Bullet.swift
+++ Watch Extension/Game/Bullet.swift
@@ -14,9 +14,12 @@ class Bullet: DynamicCircularObject {
     /**
      Initializes Bullet. Represents the shot taken by Player to destroy a Ball
     */
-    init() {
+    init(radius: CGFloat = 0, position:CGPoint = CGPoint.zero, velocity: CGVector = CGVector.zero, acceleration: CGVector = CGVector.zero) {
         
-        super.init(sprite: SKNode.init(), radius: 0, position: CGPoint.zero, velocity: CGVector.zero, acceleration: CGVector.zero)
+        let spitTexture = SKTexture(imageNamed: "spit")
+        let sprite = SKSpriteNode(texture: spitTexture)
+        
+        super.init(sprite: sprite, radius: radius, position: position, velocity: velocity, acceleration: acceleration)
         
     }
     
