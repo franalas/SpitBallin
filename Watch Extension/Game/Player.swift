0593507ad21b7b5b22fa10bcaff04f0e041d6ce4
Liam Stevenson <liam923@verizon.net>
2019-01-24 22:19:16 -0500

    Finished Player

--- Watch Extension/Game/Player.swift
+++ Watch Extension/Game/Player.swift
@@ -11,20 +11,32 @@ import SpriteKit
 
 class Player: CircularObject {
     
+    private static let HEIGHT: CGFloat = 0.1
+    
     var sprite: SKNode
     var radius: CGFloat
-    var position: CGPoint { return CGPoint.zero }
+    var position: CGPoint { return self.sprite.position }
     
     /// The x-position of `self` in the scene
     var xPosition: CGFloat {
-        get { return 0 }
-        set { }
+        get { return self.sprite.position.x }
+        set { self.sprite.position = CGPoint(x: newValue, y: self.sprite.position.y) }
     }
     
     /// The character used for `self`
     var character: Person {
-        didSet { }
+        didSet {
+            self.standardTexture = SKTexture(imageNamed: character.normalImage())
+            self.shootTexture = SKTexture(imageNamed: character.shootImage())
+            (self.sprite as! SKSpriteNode).texture = self.standardTexture
+        }
     }
+    
+    /// The texture of the sprite when not shooting
+    private var standardTexture: SKTexture
+    
+    /// The texture of the sprite when shooting
+    private var shootTexture: SKTexture
     
     /**
      Initializes a `Player` object with a given character and screen bounds
@@ -32,6 +44,22 @@ class Player: CircularObject {
         - person: The character that the player should be
         - frame: The bounds of the screen
     */
-    init(person: Person, frame: CGRect) { self.character = person; self.sprite = SKNode(); self.radius = 0 }
+    init(person: Person, frame: CGRect, height: CGFloat = Player.HEIGHT) {
+        
+        self.character = person
+        
+        self.standardTexture = SKTexture(imageNamed: person.normalImage())
+        self.shootTexture = SKTexture(imageNamed: person.shootImage())
+        
+        let width = height * standardTexture.size().width / standardTexture.size().height
+        
+        let sprite = SKSpriteNode(texture: self.standardTexture, size: CGSize(width: width, height: height))
+        self.sprite = sprite
+        
+        self.radius = sprite.size.height / 2
+        
+        self.sprite.position = CGPoint(x: frame.midX, y: frame.minY + self.radius)
+        
+    }
     
 }
